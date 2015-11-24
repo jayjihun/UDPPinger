6e9a1e7b5a4692112dcd298f5058b5a6f0772b71
Jihun-Desktop <t080205@gmail.com>
2015-11-24 20:37:42 +0900

    first started.

---
+++ UDPPinger/src/PingServer.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class PingServer
+{
+	private static final double LOSS_RATE=0.3;
+	private static final int AVERAGE_DELAY = 100;//ms
+	
+	public static void main(String[] args) throws Exception
+	{
+		
+	}
+}
